78c0b3dd058f189bc3777a0651f9bf9daf1d9498
brammont <brayanmazuera1@gmail.com>
2021-03-10 20:03:28 -0500

    finish component to adding task

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
+import React, {Component} from 'react';
 import './App.css';
 import Navigation from './components/Navigation';
 import AddingTask from './components/AddingTask';
 import ViewTask from './components/ViewTask';
-function App() {
-  return (
-    <div className="App">
-      <div className="App-header" >
-        <Navigation title="Task App"/>
-      </div>
-      <div className="App-body">
-        <section>
-          <AddingTask />       
-        </section>
-        <section>
-          <ViewTask />
-        </section>
-        <footer class="footer mt-auto">
-          <p>brammont</p>
-      </footer>
-      </div>
-      
+import { todos } from './todos.json';
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      todos
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header" >
+          <Navigation title="Task App"/>
+        </div>
+        <div className="App-body">
+          <section>
+            <AddingTask />       
+          </section>
+          <section>
+            <ViewTask />
+          </section>
+          <footer class="footer mt-auto">
+            <p>brammont</p>
+          </footer>
+        </div>      
     </div>
-  );
-}
+    )
+  }
+}  
+
 
 export default App;
--- src/components/AddingTask.js
+++ src/components/AddingTask.js
@@ -1,38 +1,68 @@
 import React, {Component} from 'react';
-import task from '../assets/completed-task.svg'
+import task from '../assets/completed-task.svg';
+import { todos } from '../todos.json';
 import './AddingTask.css';
 class AddingTask extends Component{
-   
+   constructor(){
+       super();
+       this.state ={
+        title:'',
+        responsible:'',
+        priority:'',
+        description:''
+
+    };
+    this.handleInputTask = this.handleInputTask.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleInputTask(e){
+        const {value, name} = e.target;
+        console.log(value, name);
+        this.setState({
+            [name]: value
+    });
+  }
+    handleSubmit(e) {
+       e.preventDefault();
+
+      }
     render(){
         return(
             <div class="container d-flex justify-content-center">
-                <div className="row m-4">
-                    <div className="col-auto">
-                        <img src={task} class="rounded float-left" height="500px" alt="img"/>
+                <div className="row mt-4 ">
+                    <div className="col-6 col-md-6">
+                        <img src={task} class="img-fluid" alt="img"/>
                     </div>
-                    <div className="col-auto">
+                    <div className="col-6 col-md-6">
                         <div className="card">
                             <div className="card-header">
-                                <h3>Task manager</h3>
-                            <form className="card-body">
+                                <h3>Task Manager</h3>
+                                <form onSubmit={this.handleSubmit} className="card-body">
                                 <div class="form-group">
-                                    <label>Name:</label>
-                                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter name"/>
+                                    <label>Title:</label>
+                                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Enter Title" name="title" onChange={this.handleInputTask}/>
                                 </div>
                                 <div class="form-group">
-                                    <label for="exampleFormControlSelect1">Priority</label>
-                                    <select class="form-control" id="exampleFormControlSelect1">
-                                        <option>Low</option>
-                                        <option>Medium</option>
+                                    <label>Responsible:</label>
+                                    <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Enter name" name="Responsible" onChange={this.handleInputTask}/>
+                                </div>
+                                <div class="form-group">
+                                <label>Priority:</label>
+                                    <select  name="priority" class="form-select" aria-label="Default select example"  onChange={this.handleInputTask}>
                                         <option>High</option>
+                                        <option>Medium</option>
+                                        <option>Low</option>
                                     </select>
                                 </div>
                                 <div class="form-group">
                                     <label for="exampleFormControlTextarea1">Description</label>
-                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
+                                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="5" name="description" onChange={this.handleInputTask}></textarea>
                                 </div>
+                                <button type="submit" className="btn btn-primary">
+                                    Add task
+                                </button>
                             </form>
-                            <a href="#" class="btn btn-primary">Add task</a>
+                            
                             </div>
                         </div>
                     </div>
